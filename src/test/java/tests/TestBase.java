93f35f97fd4325a780c0d419654d552c69da8fbc
Terekhov Artur <terehov.rtr@gmail.com>
2026-01-12 01:32:18 +0300

    added some fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -36,6 +37,7 @@ public class TestBase {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("allureListener", new AllureSelenide());
+        Selenide.open("/");
     }
 
     @AfterEach
